c7d49e734ce6b50796a02ff1227d279b1e8a81bf
yamaki-egg <gyt3218@gmail.com>
2023-04-13 04:23:01 +0900

    strategy

---
+++ SoundLoop/Models/FormatsData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoundLoop.Models
+{
+	internal struct FormatsData
+	{
+		public const string MP4 = ".mp4";
+		public const string MP3 = ".mp3";
+		public const string WAV = ".wav";
+	}
+}
