a55f3f58061537fd5f0edfc33f3ba42e0005af43
ni1212 <uni202212_12@telework.to>
2023-04-14 16:36:07 +0900

    mp3からwav

--- SoundLoop/View/Form1.cs
+++ SoundLoop/View/Form1.cs
@@ -2,23 +2,24 @@ using SoundLoop.Controller;
 
 namespace SoundLoop
 {
-	public partial class Form1 : Form
-	{
-		Form1Events _form1Events;
-		public Form1()
-		{
-			InitializeComponent();
+    public partial class Form1 : Form
+    {
+        Form1Events _form1Events;
+        public Form1()
+        {
+            InitializeComponent();
 
-			_form1Events = new(fnameStripStatusLabel, volumeTrackBar, this);
+            _form1Events = new(fnameStripStatusLabel, volumeTrackBar, this);
 
-			openToolStripMenuItem.Click += _form1Events.FormOpen_Click;
-			playToolStripMenuItem.Click += _form1Events.Form1Play_Click;
-			stopToolStripMenuItem.Click += _form1Events.Form1Pause_Click;
-			volumeTrackBar.ValueChanged += _form1Events.Form1Volume_Change;
-			mp4Tomp3ToolStripMenuItem.Click += _form1Events.MP4ToMP3Convert_Click;
-			//this.KeyDown += Form1Events.Form1Close_Key;
-		}
+            openToolStripMenuItem.Click += _form1Events.FormOpen_Click;
+            playToolStripMenuItem.Click += _form1Events.Form1Play_Click;
+            stopToolStripMenuItem.Click += _form1Events.Form1Pause_Click;
+            volumeTrackBar.ValueChanged += _form1Events.Form1Volume_Change;
+            mp4Tomp3ToolStripMenuItem.Click += _form1Events.MP4ToMP3Convert_Click;
+            mp3ToWavToolStripMenuItem.Click += _form1Events.MP3ToWavConvert_Click;
+            //this.KeyDown += Form1Events.Form1Close_Key;
+        }
 
 
-	}
+    }
 }
